84f87b166161516cd1729c5c1ffa2152e847eb14
ARPHAM <oaichua42@gmail.com>
2026-01-28 23:49:16 +0700

    ARPHAM

---
+++ app/(Bob)/ARPHAM/page.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+export default function ARPHAMPage() {
+  return (
+    <div>
+      <h1>ARPHAM Page</h1>
+      <p>Welcome to the ARPHAM section of the app.</p>
+    </div>
+  );
+}
